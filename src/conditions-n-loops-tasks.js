9dd6c78749427a556137f08ab29da2730711d9a7
nastya3323 <gomes-96@mail.ru>
2025-11-14 17:04:42 +0400

    feat: implement convertToRomanNumerals

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -125,8 +125,58 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  let result = '';
+
+  const tens = Math.floor(num / 10);
+  const units = Math.floor(num % 10);
+
+  if (tens === 1) {
+    result += 'X';
+  }
+
+  if (tens === 2) {
+    result += 'XX';
+  }
+
+  if (tens === 3) {
+    result += 'XXX';
+  }
+
+  switch (units) {
+    case 1:
+      result += 'I';
+      break;
+    case 2:
+      result += 'II';
+      break;
+    case 3:
+      result += 'III';
+      break;
+    case 4:
+      result += 'IV';
+      break;
+    case 5:
+      result += 'V';
+      break;
+    case 6:
+      result += 'VI';
+      break;
+    case 7:
+      result += 'VII';
+      break;
+    case 8:
+      result += 'VIII';
+      break;
+    case 9:
+      result += 'IX';
+      break;
+    default:
+      result += '';
+      break;
+  }
+
+  return result;
 }
 
 /**

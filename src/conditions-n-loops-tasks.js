54a00902188a58e3f676b2470962555ed75993df
nastya3323 <gomes-96@mail.ru>
2025-11-14 15:01:38 +0400

    feat: implement isIsoscelesTriangle

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -99,8 +99,16 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a <= 0 || b <= 0 || c <= 0) {
+    return false;
+  }
+
+  if (a + b > c && b + c > a && a + c > b) {
+    return a === b || a === c || b === c;
+  }
+
+  return false;
 }
 
 /**
